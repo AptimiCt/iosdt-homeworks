f06455e38bfbd112cd1803b7213b77eaaf333996
Vostrikov Aleksandr <aptimict11@gmail.com>
2021-12-06 03:57:09 +0300

    Добавление кнопки в InfoViewController, добавление UIAlertController и нажатия на кнопки в уведомлении.

--- Navigation/InfoViewController.swift
+++ Navigation/InfoViewController.swift
@@ -12,5 +12,37 @@ class InfoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemTeal
+        setupButton()
+        
+    }
+    
+    func setupButton(){
+        let alertButton = UIButton(frame: CGRect(x: self.view.bounds.width/2 - 100,
+                                                 y: self.view.bounds.height/2 - 25,
+                                                 width: 200,
+                                                 height: 50))
+        alertButton.setTitle("Alert", for: .normal)
+        alertButton.setTitleColor(.black, for: .normal)
+        alertButton.backgroundColor = .red
+        alertButton.layer.cornerRadius = 15
+        alertButton.addTarget(self, action: #selector(setupAlert), for: .touchUpInside)
+        self.view.addSubview(alertButton)
+    }
+    
+    @objc func setupAlert(){
+        let title = "Уведомление!"
+        let message = "Нажата кнопка в InfoViewController"
+        let alert = UIAlertController(title: title,
+                                      message: message,
+                                      preferredStyle: .alert)
+        let actionCancel = UIAlertAction(title: "Cancel", style: .destructive) { _ in
+            print("Нажата кнопка Cancel")
+        }
+        let actionOk = UIAlertAction(title: "Ok", style: .default) { _ in
+            print("Нажата кнопка Ок")
+        }
+        alert.addAction(actionCancel)
+        alert.addAction(actionOk)
+        self.present(alert, animated: true, completion: nil)
     }
 }
