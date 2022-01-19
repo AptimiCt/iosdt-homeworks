c80f46f478ec660d1c236133367a70d32fba7cd2
Vostrikov Aleksandr <aptimict11@gmail.com>
2022-01-20 02:11:53 +0300

    Обработка события появления и скрытия клавиатуры

--- Navigation/Profile/LogInViewController.swift
+++ Navigation/Profile/LogInViewController.swift
@@ -82,6 +82,17 @@ class LogInViewController: UIViewController {
         setupConstrains()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
     func setupView(){
         scrollView.toAutoLayout()
         contentView.toAutoLayout()
@@ -92,6 +103,8 @@ class LogInViewController: UIViewController {
         
         view.addSubviews(scrollView)
         scrollView.addSubviews(contentView)
+        scrollView.keyboardDismissMode = .interactive
+        
         stackView.addArrangedSubview(loginTextView)
         stackView.addArrangedSubview(passwordTextView)
         contentView.addSubviews(logoImageView, stackView, loginButton)
@@ -145,4 +158,16 @@ class LogInViewController: UIViewController {
     @objc func loginButtonTapped(){
         
     }
+    
+    @objc func keyboardWillShow(notification: NSNotification){
+        guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else { return }
+        
+        scrollView.contentInset.bottom = keyboardSize.height
+        scrollView.verticalScrollIndicatorInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height, right: 0)
+    }
+    @objc func keyboardWillHide(notification: NSNotification){
+        scrollView.contentInset.bottom = .zero
+        scrollView.verticalScrollIndicatorInsets = .zero
+    }
+    
 }
