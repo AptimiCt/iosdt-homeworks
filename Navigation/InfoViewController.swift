13b9d574e99f3c988cc18d1a02ca96dbaf5fcef8
Vostrikov Aleksandr <double.chanel0@gmail.com>
2022-09-30 00:17:24 +0300

    Добавление метода openSecond(with stringUrl) и planetLabel на InfoViewController. Рефакторинг кода.

--- Navigation/InfoViewController.swift
+++ Navigation/InfoViewController.swift
@@ -19,23 +19,34 @@ final class InfoViewController: UIViewController {
         firstLabel.textAlignment = .center
         return firstLabel
     }()
+    private let planetLabel: UILabel = {
+        let planetLabel = UILabel()
+        planetLabel.backgroundColor = .orange
+        planetLabel.layer.cornerRadius = 15
+        planetLabel.clipsToBounds = true
+        planetLabel.numberOfLines = 0
+        planetLabel.textAlignment = .center
+        return planetLabel
+    }()
     private var firstUrl = "https://jsonplaceholder.typicode.com/todos/21"
-    
+    private var planetUrl = "https://swapi.dev/api/planets/1"
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemTeal
         setupView()
         setupConstraints()
-        open(with: firstUrl)
+        openFirst(with: firstUrl)
+        openSecond(with: planetUrl)
     }
     
     private func setupView(){
         alertButton.toAutoLayout()
         firstLabel.toAutoLayout()
+        planetLabel.toAutoLayout()
         alertButton.backgroundColor = .red
         alertButton.layer.cornerRadius = 15
         
-        self.view.addSubviews(alertButton, firstLabel)
+        self.view.addSubviews(alertButton, firstLabel, planetLabel)
         
         alertButton.action = { [weak self] in
             let title = "Уведомление!"
@@ -68,18 +79,41 @@ final class InfoViewController: UIViewController {
             firstLabel.topAnchor.constraint(equalTo: alertButton.bottomAnchor, constant: 20),
             firstLabel.trailingAnchor.constraint(equalTo: alertButton.trailingAnchor),
             firstLabel.heightAnchor.constraint(greaterThanOrEqualToConstant: 40),
+            
+            planetLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            planetLabel.leadingAnchor.constraint(equalTo: alertButton.leadingAnchor),
+            planetLabel.topAnchor.constraint(equalTo: firstLabel.bottomAnchor, constant: 20),
+            planetLabel.trailingAnchor.constraint(equalTo: alertButton.trailingAnchor),
+            planetLabel.heightAnchor.constraint(greaterThanOrEqualToConstant: 40),
         ])
     }
     
-    private func open(with stringUrl: String){
+    private func openFirst(with stringUrl: String){
         guard let url = URL(string: stringUrl) else { return }
         URLSession.shared.dataTask(with: url) { [weak self] data, _, _ in
             if let data = data {
                 do {
-                    let serializationDict = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
+                    let serializationDict = try JSONSerialization.jsonObject(with: data, options: [])
                     if let dict = serializationDict as? [String: Any], let title = dict["title"] as? String {
                         DispatchQueue.main.async {
                             self?.firstLabel.text = title
+                        }
+                    }
+                } catch let error {
+                    print(error.localizedDescription)
+                }
+            }
+        }.resume()
+    }
+    private func openSecond(with stringUrl: String){
+        guard let url = URL(string: stringUrl) else { return }
+        URLSession.shared.dataTask(with: url) { [weak self] data, _, _ in
+            if let data = data {
+                do {
+                    let serializationDict = try JSONSerialization.jsonObject(with: data, options: [])
+                    if let dict = serializationDict as? [String: Any], let title = dict["name"] as? String {
+                        DispatchQueue.main.async {
+                            self?.planetLabel.text = title
                         }
                     }
                 } catch let error {
