7a3276c2308be5227d6991667847b4645a0d443e
Vostrikov Aleksandr <double.chanel0@gmail.com>
2022-12-25 17:03:34 +0300

    Добавление FavoritesViewController вкладки в таббарконтроллер. С реализацией отображения постов. Рефакторинг кода.

---
+++ Navigation/Favorites/FavoritesViewController.swift
@@ -0,0 +1,102 @@
+//
+//  FavoritesViewController.swift
+//  Navigation
+//
+//  Created by Александр Востриков on 25.12.2022.
+//
+
+import UIKit
+import StorageService
+
+class FavoritesViewController: UIViewController {
+    
+    
+    //MARK: - vars
+    private let tabBarItemProfileView = UITabBarItem(title: "Favorites",
+                                                     image: UIImage(systemName: "star.fill"),
+                                                     tag: 2)
+    
+    let tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.toAutoLayout()
+        return tableView
+    }()
+    
+    var localStorage: [Post] = []
+    
+    //MARK: - init
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        #if DEBUG
+        view.backgroundColor = .systemGray6
+        #else
+        view.backgroundColor = .red
+        #endif
+        self.tabBarItem = tabBarItemProfileView
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - override funcs
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        localStorage = Storage.posts
+        setupView()
+        addFavoriteAction()
+    }
+    //MARK: - funcs
+    private func setupView() {
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.separatorInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+        configureConstraints()
+    }
+    
+    private func configureConstraints(){
+        view.addSubview(tableView)
+        tableView.register(PostTableViewCell.self, forCellReuseIdentifier: Cells.cellForPost)
+        let constraints: [NSLayoutConstraint] = [
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(constraints)
+    }
+    
+    @objc private func addFavorite(sender: UITapGestureRecognizer){
+
+        
+    }
+    
+    private func addFavoriteAction(){
+        
+         
+    }
+}
+
+//MARK: - extensions
+extension FavoritesViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        localStorage.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: Cells.cellForPost) as? PostTableViewCell else { return UITableViewCell() }
+        cell.post = localStorage[indexPath.row]
+        return cell
+    }
+}
+
+extension FavoritesViewController: UITableViewDelegate {
+
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        tableView.cellForRow(at: indexPath)?.selectionStyle = .none
+//
+//    }
+}
